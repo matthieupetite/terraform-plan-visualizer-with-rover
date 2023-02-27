ded3f2456b46de6846f1d2ef569a73419855dd7d
Matthieu PETITE <matthieu.petite@gmail.com>
2023-02-27 13:23:26 +0100

    Update cicd.yml

--- src/main.tf
+++ src/main.tf
@@ -1,23 +1,23 @@
-resource azurerm_resource_group "rg" {
+resource "azurerm_resource_group" "rg" {
   name     = "my-rg"
-  location = "North Europe"
+  location = var.location
 }
 
-resource azurerm_virtual_network "vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "my-vnet"
   location            = "North Europe"
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["192.168.0.0/24"]
 }
 
-resource azurerm_subnet  "subnet" {
+resource "azurerm_subnet" "subnet" {
   name                 = "my-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
+  address_prefixes       = ["192.168.0.0/24"]
 }
 
-resource azurerm_network_interface "nic" {
+resource "azurerm_network_interface" "nic" {
   name                = "my-nic"
   location            = "North Europe"
   resource_group_name = azurerm_resource_group.rg.name
@@ -30,7 +30,7 @@ resource azurerm_network_interface "nic" {
   }
 }
 
-resource azurerm_network_security_group "nsg" {
+resource "azurerm_network_security_group" "nsg" {
   name                = "my-nsg"
   location            = "North Europe"
   resource_group_name = azurerm_resource_group.rg.name
@@ -48,7 +48,7 @@ resource azurerm_network_security_group "nsg" {
   }
 }
 
-resource azurerm_public_ip "pip" {
+resource "azurerm_public_ip" "pip" {
   name                = "my-pip"
   location            = "North Europe"
   resource_group_name = azurerm_resource_group.rg.name
@@ -56,7 +56,7 @@ resource azurerm_public_ip "pip" {
 }
 
 
-resource azurerm_linux_virtual_machine "vm" {
+resource "azurerm_linux_virtual_machine" "vm" {
   name                = "my-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = "North Europe"
