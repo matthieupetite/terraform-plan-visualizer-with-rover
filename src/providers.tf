b6c115a54a8914a1e0249982487bcca8a1267756
Matthieu Petite <matthieu.petite@orange.com>
2023-02-27 15:01:14 +0000

    fix cicd

--- src/providers.tf
+++ src/providers.tf
@@ -1,16 +1,16 @@
 terraform {
-    required_version = ">= 0.12"
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~>3.39.1"
-        }
+  required_version = ">= 0.12"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.39.1"
     }
+  }
 
-    backend azurerm {
-    }
+  backend "azurerm" {
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
